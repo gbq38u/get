1478fe6e175dd7b6e82fb5eb5e02fc4ba72ffcb4
gbq38u <gbmailp@gmail.com>
2026-02-12 10:14:50 +0300

    adding auto-light.py

---
+++ get-led/auto-light.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+led = 26
+photo = 6
+
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(photo, GPIO.IN)
+
+while True:
+    GPIO.output(led, 1 - GPIO.input(photo))
