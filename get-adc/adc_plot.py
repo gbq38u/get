73107b13bfcaeda9a88c8da1f2390baae8d42899
gbq38u <gbmailp@gmail.com>
2026-03-12 08:59:07 +0300

    graph+gist

--- get-adc/adc_plot.py
+++ get-adc/adc_plot.py
@@ -11,3 +11,22 @@ def plot_voltage_vs_time(time, voltage, max_voltage):
     plt.ylim(0, max_voltage)
     plt.grid(True)
     plt.show()
+
+
+def plot_sampling_period_hist(time):
+    sampling_periods = []
+
+    for i in range(1, len(time)):
+        sampling_periods.append(time[i] - time[i-1])
+
+    plt.figure(figsize=(10, 6))
+    plt.hist(sampling_periods)
+
+    plt.title("Sampling period distribution")
+    plt.xlabel("Sampling period (s)")
+    plt.ylabel("Number of measurements")
+
+    plt.xlim(0, 0.06)
+
+    plt.grid(True)
+    plt.show()
