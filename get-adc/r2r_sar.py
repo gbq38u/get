38b71a853778d0f4c49b1df3d7d16da856636b86
gbq38u <gbmailp@gmail.com>
2026-03-12 09:28:34 +0300

    adc pp graph+gits

--- get-adc/r2r_sar.py
+++ get-adc/r2r_sar.py
@@ -23,8 +23,19 @@ finally:
 
 
 
-#График напряжений на входе АЦП ПП
-""" 
+#График и гистор=грамма напряжений на входе АЦП ПП
+
+
+"""import time
+from r2r_adc import R2R_ADC
+from adc_plot import plot_voltage_vs_time, plot_sampling_period_hist
+
+voltage_values = []
+time_values = []
+duration = 3.0
+
+adc = R2R_ADC(dynamic_range=3.3)
+
 try:
     start_time = time.time()
 
@@ -36,6 +47,7 @@ try:
         time_values.append(current_time)
 
     plot_voltage_vs_time(time_values, voltage_values, adc.dynamic_range)
+    plot_sampling_period_hist(time_values)
 
 finally:
-    adc.close() """
+    adc.close()"""
