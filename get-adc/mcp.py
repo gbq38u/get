7e04b70e51bff3ed04a4562dfa6f105cca40dfaf
gbq38u <gbmailp@gmail.com>
2026-03-12 12:15:48 +0300

    graph+hist mcp

--- get-adc/mcp.py
+++ get-adc/mcp.py
@@ -1,31 +1,30 @@
 import time
-import smbus
-from adc_plot import plot_voltage_vs_time
-
-bus = smbus.SMBus(1)
-address = 0x4D   
+from mcp3021 import MCP3021
+from adc_plot import plot_voltage_vs_time, plot_sampling_period_hist
 
 voltage_values = []
 time_values = []
+
 duration = 3.0
-vref = 3.3
+dynamic_range = 5.0
 
-def read_voltage():
-    data = bus.read_i2c_block_data(address, 0, 2)
-    value = ((data[0] << 8) + data[1]) >> 2
-    voltage = value / 1023 * vref
-    return voltage
-
+adc = None
 
 try:
+    adc = MCP3021(dynamic_range)
+
     start_time = time.time()
 
     while time.time() - start_time < duration:
-        voltage = read_voltage()
+        voltage = adc.get_voltage()
         current_time = time.time() - start_time
 
         voltage_values.append(voltage)
         time_values.append(current_time)
 
+    plot_voltage_vs_time(time_values, voltage_values, dynamic_range)
+    plot_sampling_period_hist(time_values)
+
 finally:
-    plot_voltage_vs_time(time_values, voltage_values, vref)
+    if adc is not None:
+        adc.deinit()
